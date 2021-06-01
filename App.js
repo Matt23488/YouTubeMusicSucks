4bbb98eb5650c8de8d29c1626166958d61b0de3b
Matthew Bryant <matt23488@gmail.com>
2021-06-01 16:59:24 -0400

    working on it

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, LogBox } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import RNAndroidAudioStore from '@yajanarao/react-native-get-music-files';
 import Permissions from 'react-native-permissions';
 import TrackPlayer from 'react-native-track-player';
 import ActionList from './components/ActionList';
@@ -39,41 +40,93 @@ export default class App extends React.Component {
 };
 
 const HomeScreen = ({ navigation, route }) => {
-  const [[artists, albums, songs], refreshMusic] = useMusicStore();
+  // const [[artists, albums, songs], refreshMusic] = useMusicStore();
 
-  const setupPromise = TrackPlayer.setupPlayer();
-  const playSong = song => {
-    setupPromise.then(async () => {
-      console.log(`playing '${song.title}'`);
-      await TrackPlayer.add({
-        id: song.id,
-        url: song.path,
-        title: song.title,
-        artist: song.artist,
-        album: song.album,
-      });
-      TrackPlayer.play();
+  // const setupPromise = TrackPlayer.setupPlayer();
+  // const playSong = song => {
+  //   setupPromise.then(async () => {
+  //     console.log(`playing '${song.title}'`);
+  //     await TrackPlayer.add({
+  //       id: song.id,
+  //       url: song.path,
+  //       title: song.title,
+  //       artist: song.artist,
+  //       album: song.album,
+  //     });
+  //     TrackPlayer.play();
+  //   });
+  // };
+
+  const [[artists, albums], setMusic] = React.useState([[], []]);
+  const [isTrackPlayerInit, setIsTrackPlayerInit] = React.useState(false);
+
+  React.useEffect(async () => {
+    const getArtists = RNAndroidAudioStore.getArtists();
+    const getAlbums = RNAndroidAudioStore.getAlbums();
+
+    Promise.all([getArtists, getAlbums]).then(setMusic);
+  });
+
+  React.useEffect(async () => {
+    await TrackPlayer.setupPlayer();
+    setIsTrackPlayerInit(true);
+  });
+
+  // const [artistAlbums, setArtistAlbums] = React.useState([]);
+  // const getAlbumsFromArtist = artist => {
+  //   RNAndroidAudioStore.getAlbums({ artist }).then(setArtistAlbums);
+  // };
+
+  // const [albumSongs, setAlbumSongs] = React.useState([]);
+  // const getSongsFromAlbum = (artist, album) => {
+  //   RNAndroidAudioStore.getSongs({ artist, album }).then(setAlbumSongs);
+  // };
+  const viewAlbumsFromArtist = artist => {
+    RNAndroidAudioStore.getAlbums({ artist }).then(albums => {
+      navigation.push('ActionList', { items: albums, getDisplayText: album => album.album, onItemPress: album => viewSongsFromAlbum(album.author, album.album) });
     });
+  };
+
+  const viewSongsFromAlbum = (artist, album) => {
+    RNAndroidAudioStore.getSongs({ artist, album }).then(songs => {
+      navigation.push('ActionList', { items: songs, getDisplayText: song => song.title, onItemPress: playSong });
+    });
+  };
+
+  // TODO: Look at https://www.npmjs.com/package/react-native-sound-player since TrackPlayer is not working properly...
+  const playSong = async song => {
+    if (!isTrackPlayerInit) return;
+
+    await TrackPlayer.add({
+      id: song.id,
+      url: song.path,
+      type: 'default',
+      title: song.title,
+      album: song.album,
+      artist: song.artist,
+    });
+
+    TrackPlayer.play();
   };
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text style={{ flex: 1 }} onPress={refreshMusic}>Load Music</Text>
+        {/* <Text style={{ flex: 1 }} onPress={refreshMusic}>Load Music</Text> */}
         <Text style={{ flex: 1 }}>Artists found: {artists.length}</Text>
         <Text style={{ flex: 1 }}>Albums found: {albums.length}</Text>
-        <Text style={{ flex: 1 }}>Songs found: {songs.length}</Text>
+        {/* <Text style={{ flex: 1 }}>Songs found: {songs.length}</Text> */}
       </View>
       <View style={styles.main}>
-        <TouchableOpacity style={styles.navItem} onPress={() => navigation.navigate('ActionList', { items: artists, getDisplayText: artist => artist.artist, onItemPress: artist => alert(JSON.stringify(artist)) })}>
+        <TouchableOpacity style={styles.navItem} onPress={() => navigation.navigate('ActionList', { items: artists, getDisplayText: artist => artist.artist, onItemPress: artist => viewAlbumsFromArtist(artist.artist) })}>
           <Text>Artists</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.navItem} onPress={() => navigation.navigate('ActionList', { items: albums, getDisplayText: album => album.album, onItemPress: album => alert(JSON.stringify(album)) })}>
+        <TouchableOpacity style={styles.navItem} onPress={() => navigation.navigate('ActionList', { items: albums, getDisplayText: album => album.album, onItemPress: album => viewSongsFromAlbum(album.author, album.album) })}>
           <Text>Albums</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.navItem} onPress={() => navigation.navigate('ActionList', { items: songs, getDisplayText: song => song.title, onItemPress: playSong })}>
+        {/* <TouchableOpacity style={styles.navItem} onPress={() => navigation.navigate('ActionList', { items: songs, getDisplayText: song => song.title, onItemPress: playSong })}>
           <Text>Songs</Text>
-        </TouchableOpacity>
+        </TouchableOpacity> */}
       </View>
     </View>
   );
--- context/MusicStore.js
+++ context/MusicStore.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import RNAndroidAudioStore from '@yajanarao/react-native-get-music-files';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+// import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const storageKey = 'ytms.context.MusicStore';
+// const storageKey = 'ytms.context.MusicStore';
 export const useMusicStore = () => {
     const [music, setMusic] = React.useState([[], [], []]);
 
-    React.useEffect(async () => {
-        const serializedMusic = await AsyncStorage.getItem(storageKey);
-        if (serializedMusic) setMusic(JSON.parse(serializedMusic));
-    });
+    // React.useEffect(async () => {
+    //     const serializedMusic = await AsyncStorage.getItem(storageKey);
+    //     if (serializedMusic) setMusic(JSON.parse(serializedMusic));
+    // });
 
     const saveMusic = async ([artists, albums, songs]) => {
         await AsyncStorage.setItem(storageKey, JSON.stringify([artists, albums, songs]));
