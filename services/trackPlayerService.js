546559e228275ad687eae2111cf73dbacba8d59c
Matthew Bryant <matt23488@gmail.com>
2021-06-02 21:02:08 -0400

    semicolon ocd

--- services/trackPlayerService.js
+++ services/trackPlayerService.js
@@ -2,22 +2,22 @@ import TrackPlayer from 'react-native-track-player';
 
 module.exports = async function () {
     TrackPlayer.addEventListener('remote-play', () => {
-      TrackPlayer.play()
-    })
+      TrackPlayer.play();
+    });
   
     TrackPlayer.addEventListener('remote-pause', () => {
-      TrackPlayer.pause()
+      TrackPlayer.pause();
     });
   
     TrackPlayer.addEventListener('remote-next', () => {
-      TrackPlayer.skipToNext()
+      TrackPlayer.skipToNext();
     });
   
     TrackPlayer.addEventListener('remote-previous', () => {
-      TrackPlayer.skipToPrevious()
+      TrackPlayer.skipToPrevious();
     });
   
     TrackPlayer.addEventListener('remote-stop', () => {
-      TrackPlayer.destroy()
+      TrackPlayer.destroy();
     });
 };
